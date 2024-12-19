ad96de88c417be10fb67cd3a74abed634f2af088
dhtan <dinghangtan@gmail.com>
2024-12-19 13:01:59 +0800

    say hello

--- src/SayHello.jl
+++ src/SayHello.jl
@@ -1,5 +1,10 @@
 module SayHello
 
+export sayhello
 # Write your package code here.
+function sayhello()
+    @info "Hello, world!, I am dhtan"
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "SayHello.jl" begin
     # Write your tests here.
+    sayhello()
 end
