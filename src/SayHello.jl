8d2370f2802e76f418bb58c6020310774ce037b0
wanggaopeng <gaopeng011207@163.com>
2025-01-02 17:39:44 +0800

    123

--- src/SayHello.jl
+++ src/SayHello.jl
@@ -1,10 +1,12 @@
 module SayHello
 
 export sayhello
+export wgp
 # Write your package code here.
 function sayhello()
     @info "Hello, world!, I am dhtan"
 end
-
-
+function wgp()
+      @info "Hello, world!, I am wgp"
+end
 end
