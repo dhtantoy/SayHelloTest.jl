0071e1db17d5d84c01683cc43dc9b9bd510caabc
gaoyi <19020231153623@stu.xmu.edu.cn>
2025-01-02 17:40:42 +0800

    add function 'gy'

--- src/SayHello.jl
+++ src/SayHello.jl
@@ -1,10 +1,14 @@
 module SayHello
 
 export sayhello
+export gy
 # Write your package code here.
 function sayhello()
     @info "Hello, world!, I am dhtan"
 end
 
+function gy()
+    @info "Hello, world!, I am gy"
+end 
 
 end
