0e4bbe59d61a450066cb146198517c4c72a6f391
Jwn17 <jun.fan.0017@gmail.com>
2025-01-02 17:40:57 +0800

    add fj()

--- src/SayHello.jl
+++ src/SayHello.jl
@@ -1,10 +1,14 @@
 module SayHello
 
 export sayhello
+export fj
 # Write your package code here.
 function sayhello()
     @info "Hello, world!, I am dhtan"
 end
 
+function fj()
+    @info "Hello, world!, I am fj"
+end
 
 end
