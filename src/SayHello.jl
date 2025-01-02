20c9ad39136146f8af9c455864ae5e9b984287a3
chenyx <chenyuxiang2613@gmail.com>
2025-01-02 17:40:54 +0800

    It's just test , adding function 'cyx'

--- src/SayHello.jl
+++ src/SayHello.jl
@@ -1,10 +1,14 @@
 module SayHello
 
 export sayhello
+export cyx
 # Write your package code here.
 function sayhello()
     @info "Hello, world!, I am dhtan"
 end
 
+function cyx()
+    @info "Hello, world!, I am cyx, I am very handsome!"
+end
 
 end
